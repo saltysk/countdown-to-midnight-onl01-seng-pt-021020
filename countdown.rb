e3e1667a66fff52bc1be3ae7e1e4545d564f0aad
Leo Kim <sotysk@gmail.com>
2020-02-20 01:23:19 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
-#write your code here
+require 'pry'
 
-def countdown
+def countdown(num)
+  while num > 0
+  # puts num.to_s + 'SECOND(S)!'
+  puts "#{num} SECOND(S)!"
+  # num = num - 1
+  num -= 1
+end
+'HAPPY NEW YEAR!'
+end
+
+def countdown_with_sleep(num)
+  while num > 0
+puts "#{num} SECOND(S)!"
+num -= 1
+
+sleep(1)
+end
+'HAPPY NEW YEAR!'
 end
